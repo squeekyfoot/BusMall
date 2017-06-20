4e3998dbe8b68b94ac879725352d43b3075f9295
David Broxton <broxtondude@gmail.com>
2017-06-20 11:54:39 -0700

    Finishes game logic/Adds welcome page

--- js/app.js
+++ js/app.js
@@ -7,6 +7,10 @@ var productNames = [];
 var questionNumber = 1;
 var alreadyUsed = [];
 
+var leftChoice;
+var middleChoice;
+var rightChoice;
+
 // Declare global functions:
 
 function Product (name, friendlyName) {
@@ -15,12 +19,18 @@ function Product (name, friendlyName) {
   this.source = 'img/' + this.name + '.jpg';
   this.timesChosen = 0;
   this.percentClicked = 0;
-  this.timesChosenMessage = 'Chosen: ' + this.timesChosen + ' times';
-  this.percentClickedMessage = 'Percent of total: ' + this.percentClicked + '%';
+  this.timesChosenMessage = '';
+  this.percentClickedMessage = '';
   allProducts.push(this);
   productNames.push(this.name);
   this.calculatePercent = function() {
     this.percentClicked = (this.timesChosen / 25) * 100;
+  };
+  this.createMessage1 = function () {
+    this.timesChosenMessage = 'Chosen: ' + this.timesChosen + ' times';
+  };
+  this.createMessage2 = function () {
+    this.percentClickedMessage = 'Percent of total: ' + this.percentClicked + '%';
   };
 }
 
@@ -69,6 +79,14 @@ function renderProduct () {
     figcaption.setAttribute('id', 'caption' + i);
     figcaption.textContent = allProducts[random].friendlyName;
 
+    if (i === 0) {
+      leftChoice = allProducts[random];
+    } else if (i === 1) {
+      middleChoice = allProducts[random];
+    } else {
+      rightChoice = allProducts[random];
+    }
+
     parentElement.appendChild(figure);
     figure.appendChild(img);
     figure.appendChild(figcaption);
@@ -79,10 +97,16 @@ function renderProduct () {
 
 renderProduct();
 
-console.log(bag.timesChosen);
+function addPoint (selection) {
+  selection.timesChosen += 1;
+}
+
+
 
 var optionA = document.getElementById('figure0');
 optionA.addEventListener('click', function(){
+  addPoint(leftChoice);
+  console.log(leftChoice.name + ' points: ' + leftChoice.timesChosen);
   for (var i = 0; i < 3; i++) {
     // var score = document.getElementsByClassName('figure0')
     var random = Math.floor(Math.random() * productNames.length);
@@ -98,10 +122,112 @@ optionA.addEventListener('click', function(){
     alreadyUsed.push(productNames[random]);
     console.log(alreadyUsed);
   }
+  leftChoice = allProducts[random];
   alreadyUsed.splice(0, 3);
   console.log(alreadyUsed);
   var counter = document.getElementById('counter');
   questionNumber += 1;
   counter.textContent = 'Question ' + questionNumber + ' of 25';
+  if (questionNumber > 25) {
+    displayResults();
+  }
   console.log(questionNumber);
 });
+
+var optionB = document.getElementById('figure1');
+optionB.addEventListener('click', function(){
+  addPoint(middleChoice);
+  console.log(middleChoice.name + ' points: ' + middleChoice.timesChosen);
+  for (var i = 0; i < 3; i++) {
+    // var score = document.getElementsByClassName('figure0')
+    var random = Math.floor(Math.random() * productNames.length);
+    while (alreadyUsed.indexOf(productNames[random]) > -1) {
+      random = Math.floor(Math.random() * productNames.length);
+      console.log('already used');
+    };
+    // var figure = document.getElementById('figure0');
+    var img = document.getElementById('img' + i);
+    img.setAttribute('src', 'img/' + productNames[random] + '.jpg');
+    var figcaption = document.getElementById('caption' + i);
+    figcaption.textContent = allProducts[random].friendlyName;
+    alreadyUsed.push(productNames[random]);
+    console.log(alreadyUsed);
+  }
+  middleChoice = allProducts[random];
+  alreadyUsed.splice(0, 3);
+  console.log(alreadyUsed);
+  var counter = document.getElementById('counter');
+  questionNumber += 1;
+  counter.textContent = 'Question ' + questionNumber + ' of 25';
+  if (questionNumber > 25) {
+    displayResults();
+  }
+  console.log(questionNumber);
+});
+
+var optionC = document.getElementById('figure2');
+optionC.addEventListener('click', function(){
+  addPoint(rightChoice);
+  console.log(rightChoice.name + ' points: ' + rightChoice.timesChosen);
+  for (var i = 0; i < 3; i++) {
+    // var score = document.getElementsByClassName('figure0')
+    var random = Math.floor(Math.random() * productNames.length);
+    while (alreadyUsed.indexOf(productNames[random]) > -1) {
+      random = Math.floor(Math.random() * productNames.length);
+      console.log('already used');
+    };
+    // var figure = document.getElementById('figure0');
+    var img = document.getElementById('img' + i);
+    img.setAttribute('src', 'img/' + productNames[random] + '.jpg');
+    var figcaption = document.getElementById('caption' + i);
+    figcaption.textContent = allProducts[random].friendlyName;
+    alreadyUsed.push(productNames[random]);
+    console.log(alreadyUsed);
+  }
+  rightChoice = allProducts[random];
+  alreadyUsed.splice(0, 3);
+  console.log(alreadyUsed);
+  var counter = document.getElementById('counter');
+  questionNumber += 1;
+  counter.textContent = 'Question ' + questionNumber + ' of 25';
+  if (questionNumber > 25) {
+    displayResults();
+  }
+  console.log(questionNumber);
+});
+
+
+// Replace everything on the screen with the results page
+
+function displayResults () {
+  parent = document.getElementById('mainPage');
+  var child = document.getElementById('surveyProducts');
+  parent.removeChild(child);
+  child = document.createElement('main');
+  child.setAttribute('id', 'surveyResults');
+  parent.appendChild(child);
+  var message = document.getElementById('counter');
+  message.textContent = 'The result of your survey are as shown below:';
+
+  for (var i = 0; i < productNames.length; i++) {
+    allProducts[i].calculatePercent();
+    allProducts[i].createMessage1();
+    allProducts[i].createMessage2();
+
+    var result = document.createElement('div');
+    child.appendChild(result);
+
+    var image = document.createElement('img');
+    image.setAttribute('src', allProducts[i].source);
+    result.appendChild(image);
+
+
+    var data1 = document.createElement('p');
+    data1.textContent = allProducts[i].timesChosenMessage;
+    result.appendChild(data1);
+
+    var data2 = document.createElement('p');
+    data2.textContent = allProducts[i].percentClickedMessage;
+    result.appendChild(data2);
+  }
+}
