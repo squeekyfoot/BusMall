0473a64f9789743775f009e44769965e325d9b97
David Broxton <broxtondude@gmail.com>
2017-06-20 14:42:36 -0700

    Added basic chart functionality

--- js/app.js
+++ js/app.js
@@ -129,7 +129,7 @@ optionA.addEventListener('click', function(){
   questionNumber += 1;
   counter.textContent = 'Question ' + questionNumber + ' of 25';
   if (questionNumber > 25) {
-    displayResults();
+    displayChart();
   }
   console.log(questionNumber);
 });
@@ -160,7 +160,7 @@ optionB.addEventListener('click', function(){
   questionNumber += 1;
   counter.textContent = 'Question ' + questionNumber + ' of 25';
   if (questionNumber > 25) {
-    displayResults();
+    displayChart();
   }
   console.log(questionNumber);
 });
@@ -191,7 +191,7 @@ optionC.addEventListener('click', function(){
   questionNumber += 1;
   counter.textContent = 'Question ' + questionNumber + ' of 25';
   if (questionNumber > 25) {
-    displayResults();
+    displayChart();
   }
   console.log(questionNumber);
 });
@@ -230,4 +230,58 @@ function displayResults () {
     data2.textContent = allProducts[i].percentClickedMessage;
     result.appendChild(data2);
   }
+}
+
+function displayChart () {
+  parent = document.getElementById('mainPage');
+  var child = document.getElementById('surveyProducts');
+  parent.removeChild(child);
+  child = document.createElement('main');
+  child.setAttribute('id', 'surveyChart');
+  parent.appendChild(child);
+  var message = document.getElementById('counter');
+  message.textContent = 'The result of your survey are as shown below:';
+  var canvas = document.createElement('canvas');
+  canvas.setAttribute('id', 'myChart');
+  canvas.setAttribute('width', '400');
+  canvas.setAttribute('height', '400');
+  child.appendChild(canvas);
+
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  });
 }
